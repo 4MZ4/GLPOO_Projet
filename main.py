2fe6661ee32d730b1cf607dbf2ccf8e155225ab8
AAMTAA <32337551+aamtaa@users.noreply.github.com>
2020-06-19 23:31:42 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 20:20:52 2020
+
+@author: Amza
+"""
+import tkinter
+from vue.menu import Menu
+
+def main():
+    window = tkinter.Tk()
+    Menu(window)
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
